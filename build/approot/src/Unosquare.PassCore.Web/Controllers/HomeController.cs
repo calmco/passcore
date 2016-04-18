8b08987d6b394dc415345600b0a48b3d2e39fe81
Ivan Kalinichenko <ivan@gsi.ua>
2016-04-18 17:02:49 +0300

    Build

---
+++ build/approot/src/Unosquare.PassCore.Web/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿namespace Unosquare.PassCore.Web.Controllers
+{
+    using Microsoft.AspNet.Mvc;
+
+    /// <summary>
+    /// This controller is simply a placeholder to redirect any non-matching URL
+    /// to provide the context of the SPA (single page application) index
+    /// Examine the routing configuration in the Startup class
+    /// </summary>
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return File("~/index.html", "text/html");
+        }
+    }
+}
---
+++ build/wwwroot/scripts/app-00-initialize.js
@@ -0,0 +1,6 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', ['ngRoute', 'ui.bootstrap', 'vcRecaptcha', 'ngPasswordStrength'])
+        .filter('unsafe', ['$sce', function ($sce) { return $sce.trustAsHtml; }]);
+})();
